0ad5613dccdeafdb2ccb17fec3a3dfcec52ee139
Baldomero Cho <don.baldo@hotmail.com>
2024-06-08 00:04:41 -0600

    vars

--- lib/themes.dart
+++ lib/themes.dart
@@ -3,118 +3,115 @@ library uiexpress.themes;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class UIEThemes{
-  static ThemeData lightTheme({
-    Map<TargetPlatform, PageTransitionsBuilder>? builders,
-    required Color colorSeed,
-    String fontFamily = 'Roboto'
-    }) => ThemeData(
-    useMaterial3: true,
-    fontFamily: fontFamily,
-    fontFamilyFallback: ['NotoSans'],
-    appBarTheme: AppBarTheme(
-      titleTextStyle: TextStyle(
-        color: Colors.black,
-        fontSize: 20,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: colorSeed, brightness: Brightness.light,
-    ),
-    cardTheme: CardTheme(
-      elevation: 0.0,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16),
-          side: BorderSide(color: Colors.grey.shade300)
-      ),
-      color: Colors.white,
-    ),
-    inputDecorationTheme: InputDecorationTheme(
-      border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(16)
-      ),
-      hintStyle: TextStyle(color: Colors.grey),
-      isDense: true,
-      contentPadding: EdgeInsets.all(16),
-    ),
-    floatingActionButtonTheme: FloatingActionButtonThemeData(
-      elevation: 0.0,
-      foregroundColor: colorSeed,
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(colorSeed),
-            foregroundColor: MaterialStateProperty.all(Colors.white),
-            elevation: MaterialStateProperty.all(0),
-            shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)))
-        )
-    ),
-    iconTheme: IconThemeData(color: colorSeed),
-    cupertinoOverrideTheme: CupertinoThemeData(
-      primaryColor: colorSeed,
-    ),
-    pageTransitionsTheme: PageTransitionsTheme(
-      builders: builders ?? {
-        TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
-        TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
-      },
-    ),
-
-  );
-  static ThemeData darkTheme({
-    Map<TargetPlatform, PageTransitionsBuilder>? builders,
-    required Color colorSeed,
-    String fontFamily = 'Roboto'
-  }) => ThemeData(
-    useMaterial3: true,
-    fontFamily: fontFamily,
-    fontFamilyFallback: ['NotoSans'],
-    appBarTheme: AppBarTheme(
-      titleTextStyle: TextStyle(
-        color: Colors.white,
-        fontSize: 20,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
-    colorScheme: ColorScheme.fromSeed(seedColor: colorSeed, brightness: Brightness.dark),
-    inputDecorationTheme: InputDecorationTheme(
-      border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(16)
-      ),
-      hintStyle: TextStyle(color: Colors.grey),
-      isDense: true,
-      contentPadding: EdgeInsets.all(16),
-    ),
-    cardTheme: CardTheme(
-      elevation: 0.0,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16),
-          side: BorderSide(color: Colors.grey.shade300)
-      ),
-      color: Colors.grey.shade900,
-    ),
-    floatingActionButtonTheme: FloatingActionButtonThemeData(
-      elevation: 0.0,
-      foregroundColor: colorSeed,
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(colorSeed),
-            foregroundColor: MaterialStateProperty.all(Colors.white),
-            elevation: MaterialStateProperty.all(0),
-            shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)))
-        )
-    ),
-    iconTheme: IconThemeData(color: colorSeed),
-    pageTransitionsTheme: PageTransitionsTheme(
-      builders: builders ?? {
-        TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
-        TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
-      },
-    ),
-
-  );
-
-
+class UIEThemes {
+  static ThemeData lightTheme(
+          {Map<TargetPlatform, PageTransitionsBuilder>? builders,
+          required Color colorSeed,
+          Color? buttonForegroundColor,
+          String fontFamily = 'Roboto'}) =>
+      ThemeData(
+        useMaterial3: true,
+        fontFamily: fontFamily,
+        fontFamilyFallback: ['NotoSans'],
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: colorSeed,
+          brightness: Brightness.light,
+        ),
+        cardTheme: CardTheme(
+          elevation: 0.0,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+              side: BorderSide(color: Colors.grey.shade300)),
+          color: Colors.white,
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+          hintStyle: const TextStyle(color: Colors.grey),
+          isDense: true,
+          contentPadding: const EdgeInsets.all(16),
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          elevation: 0.0,
+          foregroundColor: colorSeed,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(colorSeed),
+                foregroundColor: MaterialStateProperty.all(
+                    buttonForegroundColor ?? Colors.white),
+                elevation: MaterialStateProperty.all(0),
+                shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100))))),
+        iconTheme: IconThemeData(color: colorSeed),
+        cupertinoOverrideTheme: CupertinoThemeData(
+          primaryColor: colorSeed,
+        ),
+        pageTransitionsTheme: PageTransitionsTheme(
+          builders: builders ??
+              {
+                TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+                TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
+              },
+        ),
+      );
+  static ThemeData darkTheme(
+          {Map<TargetPlatform, PageTransitionsBuilder>? builders,
+          required Color colorSeed,
+          Color? buttonForegroundColor,
+          String fontFamily = 'Roboto'}) =>
+      ThemeData(
+        useMaterial3: true,
+        fontFamily: fontFamily,
+        fontFamilyFallback: const ['NotoSans'],
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: colorSeed, brightness: Brightness.dark),
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+          hintStyle: const TextStyle(color: Colors.grey),
+          isDense: true,
+          contentPadding: const EdgeInsets.all(16),
+        ),
+        cardTheme: CardTheme(
+          elevation: 0.0,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+              side: BorderSide(color: Colors.grey.shade300)),
+          color: Colors.grey.shade900,
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          elevation: 0.0,
+          foregroundColor: colorSeed,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(colorSeed),
+                foregroundColor: MaterialStateProperty.all(
+                    buttonForegroundColor ?? Colors.white),
+                elevation: MaterialStateProperty.all(0),
+                shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100))))),
+        iconTheme: IconThemeData(color: colorSeed),
+        pageTransitionsTheme: PageTransitionsTheme(
+          builders: builders ??
+              {
+                TargetPlatform.android:
+                    const FadeUpwardsPageTransitionsBuilder(),
+                TargetPlatform.iOS: const FadeUpwardsPageTransitionsBuilder(),
+              },
+        ),
+      );
 }
